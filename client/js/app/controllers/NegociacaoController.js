84c366afe66d856125b3924f7f8b7ad9c56e03f6
Felipe de Queiroz <fe.dqueiroz@gmail.com>
2018-10-04 08:40:24 -0300

    aula 03-09

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,13 +9,20 @@ class NegociacaoController {
         this._inputValor = $("#valor");
     }
 
-    adiciona() {
+    adiciona(event) {
         
         event.preventDefault();
-        
-        console.log(typeof(this._inputData));
 
-        let data = new Date(this._inputData.value.replace(/-/g, ","));
-        console.log(data);
+        let data = new Date(...
+            this._inputData.value
+            .split('-')
+            .map((item, indice) => item - indice % 2)
+        );
+        
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        )
     }
 }
